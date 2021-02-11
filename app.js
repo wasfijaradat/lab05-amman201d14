553901291394305a43894af2bf25e700a46ef008
wasfijaradat <wasfijar@gmail.com>
2021-02-11 15:04:11 +0200

    mulip

--- app.js
+++ app.js
@@ -29,13 +29,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) {
-  
+  var z=a*b
+  var string='The product of ' + a + ' and ' + b + ' is ' + z + '.'
   //eslint-disable-line
- 
+ return[z,string]
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
